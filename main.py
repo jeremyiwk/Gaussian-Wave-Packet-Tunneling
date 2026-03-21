ec0f10f6d0003c0c584066765d6bf69d6f569be0
jeremyiwk <jeremy@micromelody.net>
2026-03-20 19:20:42 -0700

    add trivial test

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from gaussian-wave-packet-tunneling!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ tests/test_init.py
@@ -0,0 +1,6 @@
+
+from quantum_tunneling import *
+import pytest
+
+def test_init():
+    assert True
